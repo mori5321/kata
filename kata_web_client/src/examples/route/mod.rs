508d853b713c4db3ce3c6b08ea5238828d9d8244
mori5321 <moriwm77@gmail.com>
2020-11-19 18:33:42 +0900

    introduce css in rust

--- kata_web_client/src/examples/route/mod.rs
+++ kata_web_client/src/examples/route/mod.rs
@@ -1,3 +1,4 @@
+use css_in_rust::style::Style;
 use yew::prelude::*;
 use yew_router::prelude::{Router, Switch};
 
@@ -32,21 +33,54 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <Router<AppRoute, ()>
-                render = Router::render(Self::switch)
+                render = Router::render(|switch: AppRoute| {
+                    match switch {
+                        AppRoute::Profile(id) => html!{
+                            {page()}
+                        },
+                        AppRoute::Index => html!{
+                            <h1>{"Index"}</h1>
+                        },
+                    }
+                })
             />
         }
     }
 }
 
-impl Model {
-    fn switch(route: AppRoute) -> Html {
-        match route {
-            AppRoute::Profile(id) => html! {
-                <h1>{ format!("Hello, No.{}", id.to_string()) }</h1>
-            },
-            AppRoute::Index => html! {
-                <h1>{"Index"}</h1>
-            },
-        }
+fn page() -> Html {
+    html! {
+        <div class=style()>
+            <h1 class="heading">{"Page Sample"}</h1>
+            <p class="paragraph">{"This is it.."}</p>
+        </div>
     }
+}
+
+fn style() -> Style {
+    let color = "black";
+    let background_color = format!("background-color: {};", color);
+
+    let style = Style::create(
+        "Page",
+        background_color
+            + r#" 
+            .heading {
+                color: white;
+                font-size: 20px;
+            }
+
+            .heading:hover {
+                color: blue;
+            }
+
+            .paragraph {
+                color: white;
+                font-size: 12px;
+            }
+        "#,
+    )
+    .unwrap();
+
+    style
 }
